c5ef2c0c2724ba146dc1dce6dbcdbfaac6e5792d
Yt <yt>
2023-04-20 17:21:34 +0800

    feat: prod config

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,6 @@
 // const domain = 'http://test.gpt:5173'
 // const backendDomain = 'http://192.168.3.128:8000'
+// const backendDomain = 'http://127.0.0.1:8003'
 const domain = 'http://www.gpt-in-life.cn'
-const backendDomain = 'http://127.0.0.1:8003'
+const backendDomain = 'http://www.gpt-in-life.cn'
 export { domain, backendDomain }
